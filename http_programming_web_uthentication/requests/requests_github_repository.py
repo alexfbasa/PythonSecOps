ca339dbb0fb460d4b7a13deb28a9eb17c0b37238
AlexBasa <alexsimple@gmail.com>
2023-11-15 23:09:47 +0200

    Http programming web authentication

--- http_programming_web_uthentication/requests/requests_github_repository.py
+++ http_programming_web_uthentication/requests/requests_github_repository.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import requests
 
-response = requests.get('https://api.github.com/users/')
+response = requests.get('https://api.github.com/users/alexfbasa')
 print(response.url)
 print(response.text)
